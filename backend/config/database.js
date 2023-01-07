5ee1e6a8358fc124288b39c4399feb4a02518013
Rajath-gatty <rajathgatty253@gmail.com>
2023-01-07 11:33:32 +0530

    database changed

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,10 +2,17 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'mysql',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'emart-ecom.mysql.database.azure.com'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'rajath'),
+      password: env('DATABASE_PASSWORD', 'emart@123'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
