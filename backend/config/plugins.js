1dd08fc4879c506f4fb6ad0f74e7d493363bc79b
Rajath-gatty <rajathgatty253@gmail.com>
2024-06-04 14:58:50 +0530

    updated packages

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,16 +1,25 @@
 module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        provider: 'strapi-provider-upload-azure-storage',
-        providerOptions: {
-          account: env('STORAGE_ACCOUNT'),
-          accountKey: env('STORAGE_ACCOUNT_KEY'),
-          serviceBaseURL: env('STORAGE_URL'),
-          containerName: env('STORAGE_CONTAINER_NAME'),
-          cdnBaseURL: env('STORAGE_CDN_URL'),
-          defaultPath: 'assets',
-          maxConcurrent: 10
-        }
-      }
-    }
-  });
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        baseUrl: env("CDN_URL"),
+        rootPath: env("CDN_ROOT_PATH"),
+        s3Options: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+          region: env("AWS_REGION"),
+          params: {
+            ACL: null,
+            Bucket: env("AWS_BUCKET"),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
--- backend/config/server.js
+++ backend/config/server.js
@@ -1,10 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  proxy:true,
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  proxy: true,
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
-  url: env('', 'https://emart-ecommerce-9m65.onrender.com')
+  url: env("", "http://localhost:1337"),
 });
-
